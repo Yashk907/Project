a99b2a59695e569215d127b6b353c3576b819544
YashK907 <yashkarande907@gmail.com>
2024-11-05 15:17:02 +0530

    commit 2

--- app/src/main/java/com/example/project2/Screen.kt
+++ app/src/main/java/com/example/project2/Screen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+//import androidx.compose.foundation.layout.FlowRowScopeInstance.weight
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -47,6 +48,8 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -62,14 +65,19 @@ fun Preview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Screen(modifier: Modifier = Modifier) {
+    val gradient=Brush.radialGradient(
+        colors = listOf(Color(0xFF3A1576), Color(0xFF21174A)),
+        center = Offset(500f, 200f),
+        radius = 700f
+    )
     Box(
         modifier = Modifier
             .fillMaxSize()
             .background(
                 brush = Brush.radialGradient(
-                    colors = listOf(Color(0xFF3B1B72), Color(0xFF221340)),
-                    center = Offset(500f,200f),
-                    radius =700f
+                    colors = listOf(Color(0xFF3A1576), Color(0xFF21174A)),
+                    center = Offset(500f, 200f),
+                    radius = 700f
                 )
 //                linearGradient(
 //                    colors = listOf(Color(0xFF3B1B72), Color(0xFF221340)), // Purple shades
@@ -103,43 +111,56 @@ fun Screen(modifier: Modifier = Modifier) {
                     modifier
                         .fillMaxWidth()
                         .fillMaxHeight(0.1f))//tabs
-                GoldCard(modifier= Modifier
+                Box(modifier = Modifier
                     .fillMaxWidth()
-                    .fillMaxHeight(0.3f))
+                    .fillMaxHeight()
+                    .background(brush = gradient)) {
+                    Column {
+                        GoldCard(modifier= Modifier
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.3f))
 
-                //List
-                LazyColumn(modifier= Modifier
-                    .background(Color(0xFF190427))
-                    .fillMaxHeight()) {
-                    item { ButtonRow() }
-                    item{Text(text = "Today",
-                        fontSize = 14.sp,
-                        textAlign = TextAlign.Start,
-                        modifier=Modifier.padding(start = 10.dp),
-                        color = Color.White)}
-                    items(datastore().list){
-                            content->
-                        ListCard(content)
+                        //List
+                        LazyColumn(modifier= Modifier
+                            .background(Color(0xFF1D1829))
+                            .fillMaxHeight()
+                        ) {
+                            item { ButtonRow(modifier.padding(vertical = 8.dp, horizontal = 4.dp)) }
+                            item{Text(text = "Today",
+                                fontFamily = FontFamily(Font(R.font.inter_bold)),
+                                fontSize = 14.sp,
+                                textAlign = TextAlign.Start,
+                                modifier=Modifier.padding(start = 10.dp),
+                                color = Color.White)}
+                            items(datastore().list){
+                                    content->
+                                ListCard(content)
+                            }
+                            item{Text(text = "Yesterday",
+                                fontFamily = FontFamily(Font(R.font.inter_bold)),
+                                fontSize = 14.sp,
+                                modifier=Modifier.padding(start = 10.dp),
+                                textAlign = TextAlign.Start,
+                                color = Color.White)}
+                            items(datastore().list){
+                                    content->
+                                ListCard(content)
+                            }
+                            item{Text(text = "4 October 2024",
+                                fontFamily = FontFamily(Font(R.font.inter_bold)),
+                                fontSize = 14.sp,
+                                textAlign = TextAlign.Start,
+                                modifier=Modifier.padding(start = 10.dp),
+                                color = Color.White)}
+                            items(datastore().list){
+                                    content->
+                                ListCard(content)
+                            }
+                        }
                     }
-                    item{Text(text = "Yesterday",
-                        fontSize = 14.sp,
-                        modifier=Modifier.padding(start = 10.dp),
-                        textAlign = TextAlign.Start,
-                        color = Color.White)}
-                    items(datastore().list){
-                            content->
-                        ListCard(content)
-                    }
-                    item{Text(text = "4 October 2024",
-                        fontSize = 14.sp,
-                        textAlign = TextAlign.Start,
-                        modifier=Modifier.padding(start = 10.dp),
-                        color = Color.White)}
-                    items(datastore().list){
-                            content->
-                        ListCard(content)
-                    }
+
                 }
+
 
             }
 
@@ -281,7 +302,9 @@ fun Tabs(modifier: Modifier = Modifier) {
 @Composable
 fun TabText(text: String,modifier: Modifier = Modifier) {
     Text(text = text,
-        fontSize = 20.sp,
+        fontFamily = FontFamily(Font(R.font.inter_bold)),
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
         textAlign = TextAlign.Center,
         modifier = Modifier.padding(vertical = 10.dp, horizontal = 30.dp))
 }
@@ -291,45 +314,96 @@ fun TabText(text: String,modifier: Modifier = Modifier) {
 fun GoldCard(modifier: Modifier = Modifier) {
     Card(colors = CardDefaults.cardColors(Color.Transparent)
         ,modifier = modifier) {
-        Column (modifier= Modifier
-            .fillMaxWidth()
-            .padding(top = 20.dp, bottom = 35.dp)){
-            Row {
-                Column {
-                    Text(text = "24K Gold in Locker",
-                        fontSize = 16.sp,
-                        color = Color(0xFFD3853B),
-                        modifier = Modifier.padding(start = 20.dp, top = 10.dp))
-                    Row ( modifier = Modifier.padding(start = 20.dp, top = 5.dp)){
-                        Text(text = "0.828gm",
-                            fontSize = 25.sp,
-                            color = Color.White,
-                            fontWeight = FontWeight.Bold,
-                        )
-                        VerticalDivider(modifier= Modifier
-                            .padding(horizontal = 10.dp)
-                            .height(30.dp))
-                        Icon(imageVector = Icons.Default.Star,
-                            contentDescription ="" ,
-                            tint = Color.White)
-                        Text(text = "1200",
-                            fontSize = 25.sp,
-                            color = Color.White,
-                            fontWeight = FontWeight.Bold)
+        Box(modifier = Modifier.background(color = Color.Transparent)){
+            Image(painter = painterResource(id = R.drawable.bg),
+                contentDescription = "Background",
+                modifier = Modifier.fillMaxSize())
+            Column {
+                Column(modifier= Modifier
+                    .fillMaxWidth()
+                    .padding(top = 20.dp, bottom = 0.dp)
+                    .fillMaxHeight(0.55f)){
+                    Row {
+                        Column {
+                            Text(text = "24K Gold in Locker",
+                                fontSize = 12.sp,
+                                fontFamily = FontFamily(Font(R.font.inter_bold)),
+                                color = Color(0xFFD3853B),
+                                modifier = Modifier.padding(start = 20.dp, top = 10.dp))
+                            Row ( modifier = Modifier.padding(start = 20.dp, top = 5.dp)){
+                                Text(text = "0.828gm",
+                                    fontFamily = FontFamily(Font(R.font.inter_bold)),
+                                    fontSize = 24.sp,
+                                    color = Color.White,
+                                    fontWeight = FontWeight.Bold,
+                                )
+                                VerticalDivider(modifier= Modifier
+                                    .padding(horizontal = 10.dp)
+                                    .height(30.dp))
+                                Icon(painter = painterResource(id = R.drawable.rupee),
+                                    modifier = Modifier
+                                        .size(25.dp)
+                                        .align(Alignment.CenterVertically),
+                                    contentDescription ="" ,
+                                    tint = Color.Gray)
+                                Text(text = "1200",
+                                    fontSize = 20.sp,
+                                    color = Color.Gray,
+                                    fontFamily = FontFamily(Font(R.font.inter_bold)),
+                                    fontWeight = FontWeight.Bold,
+                                    modifier = Modifier.align(Alignment.CenterVertically))
+
+                            }
+
+                        }
+                        Box(modifier = Modifier
+                            .background(color = Color.Transparent)
+                            .fillMaxWidth()
+                            .fillMaxHeight()
+                            .padding(bottom = 10.dp)){
+                            Image(painter = painterResource(id = R.drawable.savingvault),
+                                contentDescription = "Vault",
+                                modifier= Modifier
+                                    .height(63.35.dp)
+                                    .width(88.dp)
+                                    .align(Alignment.BottomCenter)
+                                    )
+                        }
 
                     }
+                }
+                Box(modifier = Modifier
+                    .fillMaxWidth()
+                    .fillMaxHeight()) {
+                    Box(modifier = Modifier
+                        .background(color = Color.Transparent)
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.5f)
+                        .padding(end = 40.dp)){
+                        Image(painter = painterResource(id = R.drawable.piller),
+                            contentDescription = "Piller",
+                            modifier= Modifier
+                                .height(79.dp)
+                                .width(88.dp)
+                                .align(Alignment.BottomEnd))
+                    }
 
+                    Button(onClick = { /*TODO*/ },modifier= Modifier
+                        .fillMaxWidth()
+                        .fillMaxHeight(0.7f)
+                        .padding(bottom = 10.dp, start = 30.dp, end = 20.dp)
+                        .align(Alignment.BottomCenter),
+                        colors = ButtonDefaults.buttonColors(Color(0xFF5A21A3)),
+                        shape = RoundedCornerShape(10.dp)
+                    ) {
+                        Text(text = "Save Manually",
+                            fontFamily = FontFamily(Font(R.font.inter_bold)))
+                    }
                 }
 
             }
         }
-        Button(onClick = { /*TODO*/ },modifier= Modifier
-            .fillMaxWidth()
-            .padding(vertical = 10.dp, horizontal = 14.dp),
-            shape = RoundedCornerShape(10.dp)
-        ) {
-            Text(text = "Save Manually")
-        }
+
     }
 }
 
@@ -339,14 +413,14 @@ fun ListCard(content : content,modifier: Modifier = Modifier) {
     Card(modifier = Modifier
         .fillMaxWidth()
         .padding(vertical = 10.dp, horizontal = 10.dp),
-        colors = CardDefaults.cardColors(Color(0xFF290A3E))) {
+        colors = CardDefaults.cardColors(Color(0xFF241F33))) {
         Row(modifier = Modifier
             .fillMaxWidth()
             .padding(
                 horizontal = 10.dp,
                 vertical = 10.dp
             )) {
-            Column(modifier=Modifier.fillMaxWidth(0.16f)) {
+            Column(modifier=Modifier.fillMaxWidth(0.17f)) {
                 Box (
                     modifier = Modifier
                         .size(50.dp)
@@ -361,25 +435,34 @@ fun ListCard(content : content,modifier: Modifier = Modifier) {
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
                     .fillMaxWidth(0.65f)){
-                Text(text = "Manual Buy",
-                    fontSize = 16.sp,
+                Text(text = content.Name,
+                    fontSize = 14.sp,
                     color = Color.White,
+                    fontFamily = FontFamily(Font(R.font.inter_bold)),
                     modifier = Modifier.align(Alignment.Start))
-                Spacer(modifier = Modifier.padding(vertical = 4.dp))
+                Spacer(modifier = Modifier.padding(vertical = 0.dp))
                 Row {
                     Icon(painter = painterResource(content.image),
                         contentDescription = "",
                         tint = Color.Green,
-                        modifier = Modifier.size(16.dp))
-                    Text(text = "7.00 PM",
+                        modifier = Modifier
+                            .size(12.dp)
+                            .align(Alignment.CenterVertically))
+                    Text(text = content.Time,
+                        fontSize = 12.sp,
                         modifier = Modifier.padding(horizontal = 3.dp),
-                        color = Color.White)
+                        fontFamily = FontFamily(Font(R.font.interrr)),
+                        color = Color.Gray)
                     Icon(painter = painterResource(id = R.drawable.dot),
                         contentDescription ="" ,
-                        tint = Color.White,
-                        modifier = Modifier.size(16.dp))
-                    Text(text = "Sep 18",
-                        color = Color.White,
+                        tint = Color.Gray,
+                        modifier = Modifier
+                            .size(12.dp)
+                            .align(Alignment.CenterVertically))
+                    Text(text = content.Date,
+                        color = Color.Gray,
+                        fontSize = 12.sp,
+                        fontFamily = FontFamily(Font(R.font.interrr)),
                         modifier = Modifier.padding(horizontal = 3.dp))
                 }
             }
@@ -395,14 +478,20 @@ fun ListCard(content : content,modifier: Modifier = Modifier) {
                             modifier = Modifier.size(20.dp))
                         Text(text = content.price,
                             color = Color.White,
+                            fontSize = 14.sp,
+                            fontFamily = FontFamily(Font(R.font.inter_bold)),
                             modifier = Modifier.align(Alignment.CenterVertically))
                     }}
                 Column(modifier = Modifier.align(Alignment.End)) {
                     Row {
                         Text(text = content.weight,
-                            color = Color.White)
-                        Text(text = "gm",
-                            color = Color.White)
+                            color = Color.Gray,
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(R.font.interrr)))
+                        Text(text = " gms",
+                            color = Color.Gray,
+                            fontSize = 12.sp,
+                            fontFamily = FontFamily(Font(R.font.interrr)))
                     }
                 }
 
@@ -441,10 +530,11 @@ fun Statusbuttoncontent(text: String,
         contentPadding = ButtonDefaults.TextButtonWithIconContentPadding,
         shape = RoundedCornerShape(10.dp),
         modifier=modifier,
-        colors = ButtonDefaults.outlinedButtonColors(Color(0xFF290A3E))) {
+        colors = ButtonDefaults.outlinedButtonColors(Color(0xFF241F33))) {
         Text(text = text,
             color = Color.White,
-            fontSize = 14.sp)
+            fontFamily = FontFamily(Font(R.font.inter_bold)),
+            fontSize = 10.sp)
         Image(painter = painterResource(id = icon),
             contentDescription = "",
             colorFilter = ColorFilter.tint(Color.White),
@@ -461,16 +551,17 @@ fun buttoncontent(text: String,
         contentPadding = ButtonDefaults.TextButtonWithIconContentPadding,
         shape = RoundedCornerShape(10.dp),
         modifier=modifier,
-        colors = ButtonDefaults.outlinedButtonColors(Color(0xFF290A3E))) {
+        colors = ButtonDefaults.outlinedButtonColors(Color(0xFF241F33))) {
 
         Image(painter = painterResource(id = icon),
             contentDescription = "",
             colorFilter = ColorFilter.tint(Color.White),
             modifier = Modifier
-                .size(14.dp)
+                .size(13.dp)
                 .padding(start = 2.dp))
         Text(text = text,
             color = Color.White,
-            fontSize = 14.sp)
+            fontFamily = FontFamily(Font(R.font.inter_bold)),
+            fontSize = 11.sp)
     }
 }
